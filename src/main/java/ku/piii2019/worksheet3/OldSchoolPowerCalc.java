cf8b3d9bb3bf59742a849845ae38d17ac1fa31c5
Joshua Moore <K1720552@kingston.ac.uk>
2019-10-14 21:28:07 +0100

    Question 3 almost complete. Just getRandomNumbers left to do.

--- src/main/java/ku/piii2019/worksheet3/OldSchoolPowerCalc.java
+++ src/main/java/ku/piii2019/worksheet3/OldSchoolPowerCalc.java
@@ -120,31 +120,83 @@ public class OldSchoolPowerCalc implements PowerCalc {
 
     @Override
     public List<Integer> removeDuplicates(List<Integer> inThisList) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Integer> result = new ArrayList<>();
+        for(Integer x : inThisList){
+            if(!result.contains(x)){
+                result.add(x);
+            }
+        }
+        System.out.println(result);
+        return result;
     }
 
     @Override
     public List<Integer> getSortedList(List<Integer> usingThisList, boolean biggestFirst) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Integer> result = new ArrayList<>(usingThisList);
+        int temp = 0;
+        for(Integer x : result){
+            if(biggestFirst == false){
+                for(Integer y : result){
+                    if(x>y){
+                        temp = x;
+                        x = y;
+                        y = temp;
+                    }
+                }
+            }else if(biggestFirst == true){
+                for(Integer a : result){
+                    if(x<a){
+                        temp = x;
+                        x = a;
+                        a = temp;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+        return result;
     }
 
     @Override
     public int getNumOccurrences(int thisNumber, List<Integer> inThisList) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int result = 0;
+        for(Integer x : inThisList){
+            if(x==thisNumber){
+                result = result + 1;
+            }
+        }
+        System.out.println(result);
+        return result;
     }
 
     @Override
     public List<Integer> addTwoLists(List<Integer> listA, List<Integer> listB) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Integer> result = new ArrayList<>();
+        for(int i=0;i<listA.size();i++){
+            result.add(listA.get(i)+listB.get(i));
+        }
+        System.out.println(result);
+        return result;
     }
 
     @Override
     public Map<Integer, Integer> mapTheseLists(List<Integer> listOfKeys, List<Integer> listOfValues) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<listOfKeys.size();i++){
+            map.put(listOfKeys.get(i), listOfValues.get(i));
+        }
+        System.out.println(map);
+        return map;
     }
 
     @Override
     public List<Integer> getRandomNumbers(int smallest, int biggest, int thisMany) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 }
